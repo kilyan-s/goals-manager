33861c0245d00065d6831cac5197a59d226cc84d
Kilyan SOCKALINGUM <sockalingum.kilyan@gmail.com>
2020-01-09 11:50:05 -0500

    UI and outlets and actions for GoalsVC.

--- goals-manager/goals-manager/Controller/GoalsVC.swift
+++ goals-manager/goals-manager/Controller/GoalsVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GoalsVC.swift
 //  goals-manager
 //
 //  Created by Kilyan SOCKALINGUM on 09/01/2020.
@@ -8,13 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class GoalsVC: UIViewController {
+    //Outlets
+    @IBOutlet weak var tableview: UITableView!
+    
+    //Variables
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    //Actions
+    @IBAction func addGoalBtnWasPressed(_ sender: Any) {
+        print("addGoalBtnWasPressed")
+    }
+    
 }
-
