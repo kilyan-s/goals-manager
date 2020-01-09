2106ff4d00948a3cf75cb1de03862ac33def664e
Kilyan SOCKALINGUM <sockalingum.kilyan@gmail.com>
2020-01-09 11:20:39 -0500

    Xcode project setup

---
+++ goals-manager/goals-manager/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goals-manager
+//
+//  Created by Kilyan SOCKALINGUM on 09/01/2020.
+//  Copyright © 2020 Kilyan SOCKALINGUM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
