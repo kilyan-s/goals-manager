6347254eb2ea5da05235414383f4f5e8243bc46e
Kilyan SOCKALINGUM <sockalingum.kilyan@gmail.com>
2020-01-10 11:15:41 -0500

    Save data to persistent storage.

--- goals-manager/goals-manager/Controller/FinishGoalVC.swift
+++ goals-manager/goals-manager/Controller/FinishGoalVC.swift
@@ -7,6 +7,10 @@
 //
 
 import UIKit
+import CoreData
+
+typealias CompletionHandler = (_ Success: Bool) -> ()
+let appDelegate = UIApplication.shared.delegate as? AppDelegate
 
 class FinishGoalVC: UIViewController {
     //Outlets
@@ -17,7 +21,7 @@ class FinishGoalVC: UIViewController {
     var goalDescription: String!
     var goalType: GoalType!
     
-    
+    //Function
     override func viewDidLoad() {
         super.viewDidLoad()
         createGoalBtn.bindToKeyboard()
@@ -28,9 +32,40 @@ class FinishGoalVC: UIViewController {
         self.goalType = type
     }
     
+    func save(_ completion: @escaping CompletionHandler){
+        //context to save to coreData
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        
+        //Create goal entity
+        let goal = Goal(context: managedContext)
+        
+        //Set attributes
+        goal.goalDescription = goalDescription
+        goal.goalType = goalType.rawValue
+        goal.goalEndValue = Int32(pointsTxtField.text!)!
+        goal.goalCurrentValue = Int32(0)
+        
+        //Try to save to coreDate otherwise throw error
+        do {
+            try managedContext.save()
+            print("Save successful")
+            completion(true)
+        } catch {
+            debugPrint("Could not save \(error.localizedDescription)")
+            completion(false)
+        }
+        
+    }
+    
     //Actions
     @IBAction func createGoalBtnWasPressed(_ sender: Any) {
-        
+        if pointsTxtField.text != "" {
+            self.save { (success) in
+                if success {
+                    self.dismiss(animated: true, completion: nil)
+                }
+            }
+        }
     }
     
     @IBAction func backBtnWasPressed(_ sender: Any) {
